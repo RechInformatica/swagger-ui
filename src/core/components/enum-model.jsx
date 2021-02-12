7ee86845f4df1ce8dc9f2eb027306029d2910a29
Mendes <mendes@rech.com.br>
2021-02-12 14:33:55 -0300

    Modificado o formato de exibição dos enumerados
    Enumerados retraídos - Antes
    Array [ 3 ]
    
    Enumerados expandidos - Antes
    [ 1 - Inativo, 2 - Ativo, 3 - Inativo por transferência de produtos(sped) ]
    
    Enumerados retraídos - Agora
    Enumerados [ 3 ]
    
    Enumerados expandidos - Agora
    1 - Inativo
    2 - Ativo
    3 - Inativo por transferência de produtos(sped)

--- src/core/components/enum-model.jsx
+++ src/core/components/enum-model.jsx
@@ -3,17 +3,21 @@ import ImPropTypes from "react-immutable-proptypes"
 
 const EnumModel = ({ value, getComponent }) => {
   let ModelCollapse = getComponent("ModelCollapse")
-  let collapsedContent = <span>Array [ { value.count() } ]</span>
+  let collapsedContent = <span>Enumerados [ {value.count()} ]<br/></span>
   return <span className="prop-enum">
     Enum:<br />
-    <ModelCollapse collapsedContent={ collapsedContent }>
-      [ { value.join(", ") } ]
-    </ModelCollapse>
+    <ModelCollapse collapsedContent={collapsedContent}>
+      {value.map(enumValue => createEnumLine(enumValue))}
+    </ModelCollapse><br/>
   </span>
 }
 EnumModel.propTypes = {
   value: ImPropTypes.iterable,
   getComponent: ImPropTypes.func
+}
+
+function createEnumLine(enumValue) {
+  return <p style={{ paddingLeft: "50px" }}>{enumValue}</p>
 }
 
 export default EnumModel
